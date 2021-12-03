069ddfe4fcba2ad4cb94092a95d82f414efe1c29
qanh1604 <ngoquocanh1604@gmail.com>
2021-12-03 15:24:09 +0700

    delete draw.io

--- FoodAdmin/models/productModel.js
+++ FoodAdmin/models/productModel.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const productSchema = new Schema({
-    id: { type: ObjectId },
-    nameProduct: { type: String },
-    price: { type: Number },
-    number: { type: Number },
-    dateSell: { type: Date},
-    description: { type: String},
-    idCategory: { type: Schema.Types.ObjectId, ref: 'Category'},
-    img: { type: String },
-    
-})
+  id: { type: ObjectId },
+  nameProduct: { type: String },
+  price: { type: Number },
+  number: { type: Number },
+  dateSell: { type: Date },
+  description: { type: String },
+  idCategory: { type: Schema.Types.ObjectId, ref: "Category" },
+  img: { type: String },
+});
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model("Product", productSchema);
